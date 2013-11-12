cc639adaad9a28e05a767e7a2d0709b6028aaac5
Neeraj Kumar <n26kumar@scslt115.cs.uwaterloo.ca>
2013-11-11 21:27:03 -0500

    some minor changes to genDriver.pl, renamed to libMyBigLib.so

--- scripts/genDriver.pl
+++ scripts/genDriver.pl
@@ -10,19 +10,21 @@ open (BIGLIB , ">scriptedDriver.cc") or die "Couldn't open driver.cc for writing
 print BIGLIB "#include <stdlib.h>\n";
 print BIGLIB "#include <stdio.h>\n";
 
-    print BIGLIB "int main()\n";
-    print BIGLIB "{\n";
-    print BIGLIB "int t;\n";
-    print BIGLIB "\n";
+print BIGLIB "int main()\n";
+print BIGLIB "{\n";
+print BIGLIB "  int t = 0;\n";
+print BIGLIB "  \n";
+
 for (my $i = 0; $i < $ARGV[0]; $i++)
 {
-	$funcNum = int(rand(100000));
-	$var1 = int(rand(100));
-	$var2 = int(rand(100));
-    print BIGLIB "extern int func$funcNum(int, int);\n";
-    print BIGLIB "t = func$funcNum($var1, $var2);\n";
-    print BIGLIB "\n";
+    $funcNum = int(rand(100000));
+    $var1 = int(rand(100));
+    $var2 = int(rand(100));
+    print BIGLIB "  extern int func$funcNum(int, int);\n";
+    print BIGLIB "  t &= func$funcNum($var1, $var2);\n";
+    print BIGLIB "  \n";
 }
-    print BIGLIB "return t;\n";
-    print BIGLIB "}\n\n";
+
+print BIGLIB "  return t;\n";
+print BIGLIB "}\n\n";
 
